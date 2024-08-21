48c4a547e73c0f30c230b71cb6842e93d0d8bfd3
Aleksmjr <Aleksmjr97@gmail.com>
2024-08-21 21:47:12 +0300

    21.08.2024

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,11 @@ burgerButton.addEventListener('click', function () {
   burgerMenu.classList.toggle('open');
   this.classList.toggle('open');
 });
+
+const burgerButton2 = document.querySelector('.burger-button');
+
+burgerButton2.addEventListener('click', function () {
+  const burgerMenu2 = document.querySelector('.burger-menu');
+  burgerMenu2.classList.remove('open');
+  this.classList.remove('open');
+});
