afd10fb930a1a8d987ac0913fb444197060f740f
Reazzyg <mudrsemen@ya.ru>
2024-09-18 23:01:17 +0300

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,43 @@ burgerBtnClose.addEventListener('click', function () {
 // technologyTab.addEventListener('click', function () {
 //   technologyTab.classList.add('active');
 // });
+let arr = ['a', 'b', 'c'];
+
+let arr2 = ['first', 'letter', 'third'];
+
+let buttons = document.querySelectorAll('.rgb');
+let colors = document.querySelectorAll('.color');
+
+// console.log(buttons);
+// console.log(colors);
+
+buttons.forEach(function (el, index) {
+  console.log(el, colors[index]);
+
+  el.addEventListener('click', function () {
+    colors.forEach(function (elem) {
+      elem.classList.remove('active');
+    });
+
+    buttons.forEach(function (elem) {
+      elem.classList.remove('active');
+    });
+
+    el.classList.add('active');
+
+    colors[index].classList.add('active');
+  });
+});
+
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+// sum(5, 6);
+
+// console.log(arr);
+
+// Массив кнопок
+// Массив картинок
+// объект.метод
+// массив.метод
+// массив.forEach
